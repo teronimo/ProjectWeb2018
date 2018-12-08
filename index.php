5b94448c7eb49db335e1c9576b11395c10c0b873
teronimo <45518345+teronimo@users.noreply.github.com>
2018-12-08 14:46:50 +0200

    no message

--- index.php
+++ index.php
@@ -2,6 +2,39 @@
 include_once 'db_connect.php';
 session_start();
 header("Content-Type: text/html; charset=utf-8");
+
+$myKml = "geonode-population_data_per_block.kml";
+$xml = simplexml_load_file($myKml);
+
+$placemarks = $xml->Document->Folder->Placemark;
+$con=mysqli_connect("localhost","root", "", "project_2018");
+
+$query = '';
+$run = '';
+for ($i = 0; $i < sizeof($placemarks); $i++)
+{
+	$coordinates = $placemarks[$i]->name;
+	$cor_d = explode(' ', $placemarks[$i]->MultiGeometry->Polygon->outerBoundaryIs->LinearRing->coordinates);
+	$qtmp=array();
+	foreach($cor_d as $value)
+	{
+		//$tmp = explode(',',$value);
+		$qtmp[] = $value ; 
+	}	
+
+	$cor_d = json_encode($qtmp);
+
+	//$query .='\''.$coordinates.'\', \''.$cor_d.'\'';
+	//$run .="INSERT INTO map (name, coordinates) VALUES ($coordinates, $cor_d);";
+	if ($run = $con->prepare("INSERT INTO map (name, coordinates) VALUES (?, ?)"))
+	{
+		$run->bind_param($coordinates, $f_d);
+		$run->execute();
+	}
+}
+//echo $run;
+//mysqli_query($con,$run);
+mysqli_close($con);
 ?>
 
 <!DOCTYPE html>
