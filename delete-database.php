db4c277ad41b1ee2f1ab62551576aaf97bc05cbf
teronimo <45518345+teronimo@users.noreply.github.com>
2019-07-01 23:37:49 +0300

    Add files via upload

--- delete-database.php
+++ delete-database.php
@@ -3,10 +3,4 @@
 	$con->query("DELETE FROM map");
 	$con->query("ALTER TABLE map AUTO_INCREMENT = 1");
 	header("Location: admin-control.php");
-=======
-<?php
-	include_once 'db_connect.php';
-	$con->query("DELETE FROM map");
-	$con->query("ALTER TABLE map AUTO_INCREMENT = 1");
-	header("Location: admin-control.php");
 ?> 
