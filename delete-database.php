a53e8f3be3769dd872106f37f074e0613caa8368
teronimo <45518345+teronimo@users.noreply.github.com>
2019-07-31 18:31:45 +0300

    μηδενίζει και τις 2 βάσεις

--- delete-database.php
+++ delete-database.php
@@ -2,5 +2,7 @@
 	include_once 'db_connect.php';
 	$con->query("DELETE FROM map");
 	$con->query("ALTER TABLE map AUTO_INCREMENT = 1");
+	$con->query("DELETE FROM simulation");
+	$con->query("ALTER TABLE simulation AUTO_INCREMENT = 1");
 	header("Location: admin-control.php");
 ?> 
