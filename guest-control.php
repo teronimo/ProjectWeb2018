ff81d86dcca9e229c8b15c226cb573e9aaf2c031
lakos20 <45520664+lakos20@users.noreply.github.com>
2019-08-01 14:06:40 +0300

    δεν υπαρχει στις επιλογες η απεικονιση στοιχειων

--- guest-control.php
+++ guest-control.php
@@ -15,6 +15,8 @@
 
     <!-- Custom styles for this template -->
     <link href="css/simple-sidebar.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css" />
+	<script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"></script>
 
 </head>
 
@@ -25,9 +27,6 @@
         <!-- Sidebar -->
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
-				<li>
-                    <a href="map-guest.php">Απεικόνιση Στοιχείων Πόλης</a>
-                </li>
                 <li>
                     <a href="#">Εκτέλεση Εξομοίωσης</a>
                 </li>
@@ -41,7 +40,42 @@
                 <h1>You are logged in as guest!</h1>
                 <a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle">Δες τις επιλογές σου!</a>
             </div>
-        </div>
+			<div id="map">
+				<html>
+					<body>
+						<div id="map" style="width: 1004px; height: 800px"></div>
+						 <script type="text/javascript"> 
+						 var map = new L.Map('map');
+						 var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+						 osmAttrib = 'Map data &copy; 2011 OpenStreetMap contributors',
+						 osm = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+						  map.setView(new L.LatLng(22.9340045, 40.6430126), 16).addLayer(osm);
+						 var popup = new L.Popup();
+
+						<?php
+							include'db_connect.php';
+
+							if($coordinates = $con->query("SELECT coordinates FROM map"))
+								{
+									?>
+									var polygonArray = [];
+									<?php 
+									while ($row = $coordinates->fetch_object())
+									{
+										?>
+										polygonArray.push([<?php echo $row->coordinates ?>]);
+										<?php
+									}				
+								} 
+						?>
+
+						console.log(polygonArray);
+						var poly = L.polygon(polygonArray, {color :"grey", fillColor: "grey"}).addTo(map);
+						poly.bindPopup('<p>Πληκτρολογίστε το πλήθος των διαθέσιμων θέσων στάθμευσης</p><form id="Park_Number" method="POST" action="" ><div class="form-group"><input type="text" class="form-control" name="Park_Number" placeholder="Park_Number"></div> <button type="submit" class="btn btn-primary">Submit</button></form>');
+						</script>
+					</body>
+				</html>
+			</div>
         <!-- /#page-content-wrapper -->
 
     </div>
